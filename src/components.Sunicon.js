eca2367f5efa9237cee98aef509fbc1b3f8e490c
HemaNakra <64476482+HemaNakra@users.noreply.github.com>
2021-06-05 12:20:10 +0530

    Update components.Sunicon.js

--- src/components.Sunicon.js
+++ src/components.Sunicon.js
@@ -1,55 +1,21 @@
-import { Button } from "react-bootstrap";
-import React, { useState } from "react";
-import { Modal } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { SET_CITY } from "../redux/weather/WeatherSlice";
+import React from "react";
 
-function CitySelector() {
-  //STATES
-  const [show, setShow] = useState(false);
-  const [cityName, setCityName] = useState("");
-
-  const dispatch = useDispatch();
-
-  const handleClose = () => {
-    dispatch(SET_CITY(cityName));
-    setShow(false);
-  };
-  const handleShow = () => setShow(true);
+function SunIconComponent(props) {
+  const alignMentType = props.align;
 
   return (
-    <>
-      <div
-        className='d-flex justify-content-center align-items-center'
-        style={{ minHeight: "300px" }}
-      >
-        <Button variant={"light"} onClick={handleShow}>
-          Select City Name
-        </Button>
-      </div>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter City Name</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input
-            type='text'
-            className='form-control'
-            placeholder='City Name here..'
-            onChange={(e) => {
-              setCityName(e.target.value);
-            }}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='primary' onClick={handleClose}>
-            Done
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <div
+      className={
+        alignMentType === "right"
+          ? "d-flex flex-row-reverse text-danger"
+          : "d-flex text-danger"
+      }
+      style={{ minWidth: "100%", height: "150px" }}
+    >
+      <i className='fas fa-sun' style={{ fontSize: "70px" }}></i>
+    </div>
   );
 }
 
-export default CitySelector;
+export default SunIconComponent;
+         
